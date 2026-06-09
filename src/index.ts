d5936828804c43cc15709086a32e98869e69033a
Rhys Bowles <3155839+rhyswfbowles@users.noreply.github.com>
2026-06-09 12:41:12 +0100

    feat: add timestamps

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { startBot } from './bot'
 import { BOT_NAME } from './config'
 import { version } from '../package.json'
 
+const _log = console.log.bind(console)
+const _error = console.error.bind(console)
+const ts = () => new Date().toISOString().replace('T', ' ').slice(0, 19)
+console.log   = (...args) => _log(`[${ts()}]`, ...args)
+console.error = (...args) => _error(`[${ts()}]`, ...args)
+
 console.log(`🤖 ${BOT_NAME} v${version} — starting`)
 
 startBot().catch(console.error)
